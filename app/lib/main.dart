e62967ce9d241d4f92b0f85c0df059beb5c39333
victoricoma <victor.icoma@etec.sp.gov.br>
2024-03-21 16:47:27 -0300

    Aula 2103 - Utilização de Containers em forma de colunas.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(new PerguntaApp());
+}
 
 class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual sua cor preferida?',
+      'Qual seu animal preferido?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Pergunta'),
         ),
-        body: Column(),
+        body: Column(children: [
+          Text(perguntas[0]),
+          const ElevatedButton(onPressed: null, child: Text('Resposta 1')),
+          const ElevatedButton(onPressed: null, child: Text('Resposta 2')),
+          const ElevatedButton(onPressed: null, child: Text('Resposta 3')),
+        ]),
       ),
     );
   }
