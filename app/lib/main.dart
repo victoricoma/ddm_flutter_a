45776fbadb3c2a7fcd4837dd71e3ac4cd9cf8df7
victoricoma <victor.icoma@etec.sp.gov.br>
2024-04-18 15:32:58 -0300

    Ajuste de Versão da Aula1104

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -4,27 +4,39 @@ void main() {
   runApp(new PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget {
-  const PerguntaApp({super.key});
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual sua cor preferida?',
-      'Qual seu animal preferido?',
-    ];
+    final perguntas = ['Qual sua cor favorita?', 'Qual seu animal favorito?'];
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Pergunta'),
-        ),
-        body: Column(children: [
-          Text(perguntas[0]),
-          ElevatedButton(onPressed: () => print('Azul Celeste'), child: Text('Resposta 1')),
-          ElevatedButton(onPressed: () => print('Vermelho Raiva'), child: Text('Resposta 2')),
-          ElevatedButton(onPressed: () => print('Amarelo Japones'), child: Text('Resposta 3'))
-        ]),
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Perguntas'),
       ),
-    );
+      body: Column(
+        children: [
+          Text(perguntas[perguntaSelecionada]),
+          ElevatedButton(onPressed: responder, child: const Text('Resposta 1')),
+          ElevatedButton(onPressed: responder, child: const Text('Resposta 2')),
+          ElevatedButton(onPressed: responder, child: const Text('Resposta 3'))
+        ],
+      ),
+    ));
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({super.key});
+  @override
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
