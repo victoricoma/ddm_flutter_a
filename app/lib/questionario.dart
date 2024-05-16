d4600e5b51be0c06f3a6ed18c964058e82286fe5
victoricoma <victor.icoma@etec.sp.gov.br>
2024-05-16 16:35:34 -0300

    Aula1605 - Componente de Resultado

---
+++ app/lib/questionario.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import './questao.dart';
+import './resposta.dart';
+
+class Questionario extends StatelessWidget {
+  const Questionario({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
