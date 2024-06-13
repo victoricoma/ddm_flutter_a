9ab21ecec1d924017c971b329b32d9b8051e3473
victoricoma <victor.icoma@etec.sp.gov.br>
2024-06-13 16:30:38 -0300

    Aula1306 corr~ção

--- app/lib/questionario.dart
+++ app/lib/questionario.dart
@@ -3,10 +3,37 @@ import './questao.dart';
 import './resposta.dart';
 
 class Questionario extends StatelessWidget {
-  const Questionario({super.key});
+  final List<Map<String, Object>> perguntas;
+  final int perguntaSelecionada;
+  final void Function(int) quandoResponder;
+
+  const Questionario({
+    required this.perguntas,
+    required this.perguntaSelecionada,
+    required this.quandoResponder,
+    super.key,
+  });
+
+  bool get temPerguntaSelecionada {
+    return perguntaSelecionada < perguntas.length;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    List<Map<String, Object>> respostas = temPerguntaSelecionada
+        ? perguntas[perguntaSelecionada]['respostas']
+            as List<Map<String, Object>>
+        : [];
+    return Column(
+      children: [
+        Questao(perguntas[perguntaSelecionada]['texto'] as String),
+        ...respostas.map((resp) {
+          return Resposta(
+            resp['texto'] as String,
+            () => quandoResponder(resp['pontuacao'] as int),
+          );
+        })
+      ],
+    );
   }
 }
